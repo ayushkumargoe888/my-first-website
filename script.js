ee1626ac78e9b926661c1ed0011cb0ae541bd540
ayushkumargoe888 <ayushkumargoel903@gmail.com>
2026-06-27 16:15:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function changeText(){
-    document.getElementById("demo").innerHTML="Congratulations! 🎉 Your JavaScript is working.";
+function changeText() {
+    document.getElementById("demo").innerHTML = `
+        <h3>🍕 Food Menu</h3>
+        <ul style="list-style:none; padding:0;">
+            <li>🍕 Pizza</li>
+            <li>🍔 Burger</li>
+            <li>🍟 French Fries</li>
+            <li>🌮 Tacos</li>
+            <li>🍦 Ice Cream</li>
+        </ul>
+    `;
 }
